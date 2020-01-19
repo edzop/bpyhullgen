2288d9611f78fd512611d9aa33fd7ab55eaab0f0
ek <ek@predator>
2020-01-19 10:40:38 +0800

    assign names to window

--- window_helper.py
+++ window_helper.py
@@ -18,7 +18,14 @@
 
 import bpy
 
+import imp
+
 import math
+
+material_helper = imp.load_source('material_helper','material_helper.py')
+
+material_bolts=material_helper.get_material_bolts()
+material_window=material_helper.get_material_window()
 
 
 def calc_arc_point_ellipse(centerpoint,angle,distance):
@@ -33,19 +40,22 @@ def calc_arc_point_circle(centerpoint,angle,distance):
 
     return (x,y)
 
-def make_circle(name,centerpoint,diameter):
-    bpy.ops.mesh.primitive_cylinder_add(radius=diameter, depth=0.4, enter_editmode=False, location=centerpoint)
+def make_circle(name,centerpoint,diameter,circle_depth):
+    bpy.ops.mesh.primitive_cylinder_add(radius=diameter, depth=circle_depth, enter_editmode=False, location=centerpoint)
     new_object=bpy.context.view_layer.objects.active
     new_object.name=name
     return new_object
 
     
-def make_window(centerpoint,diameter):
-
+def make_window(name="window",centerpoint=(0,0,0),diameter=1,depth=0.4):
 
     ellipse_ratio=2
 
-    main_circle=make_circle("main_window",centerpoint,diameter)
+    window_name="window_%s"%name
+
+    main_circle=make_circle(window_name,centerpoint,diameter,depth)
+
+    material_helper.assign_material(main_circle,material_window)
 
     bpy.ops.transform.resize(value=(ellipse_ratio, 1, 1), 
         constraint_axis=(True, True, False))
@@ -61,7 +71,11 @@ def make_window(centerpoint,diameter):
         arc_point=calc_arc_point_ellipse(centerpoint,a,
             [diameter*2*ellipse_ratio+bolt_margin,diameter*2+bolt_margin]
             )
-        bolt_hole=make_circle("bolt",(arc_point[0],arc_point[1],centerpoint[2]),bolt_diameter)
+        bolt_hole=make_circle("bolt",(arc_point[0],arc_point[1],centerpoint[2]),bolt_diameter,depth)
         bolt_hole.parent=main_circle
+
+        material_helper.assign_material(bolt_hole,material_bolts)
+
+    return main_circle
 
 
