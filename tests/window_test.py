1719bb98c7bd8b40b61334a0ac69b79c2b3b65f7
ek <ek@predator>
2020-01-19 10:40:17 +0800

    named windows

--- tests/window_test.py
+++ tests/window_test.py
@@ -6,4 +6,4 @@ window_helper = imp.load_source('window_helper','window_helper.py')
 window_helper.make_window(centerpoint=(-5,-3,0),diameter=2)
 window_helper.make_window(centerpoint=(4,-3,0),diameter=1)
 
-window_helper.make_window(centerpoint=(0,3,0),diameter=3)
+window_helper.make_window("w3",centerpoint=(0,3,0),diameter=3)
