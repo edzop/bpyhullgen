5cd13d78f468f370e37f89c7d440b42e4e377880
ek <ek@predator>
2020-02-07 13:43:13 +0800

    fix demo

--- tests/multiple_curves.py
+++ tests/multiple_curves.py
@@ -3,6 +3,8 @@ import bpy
 from bpyhullgen.hullgen import curve_helper
 from bpyhullgen.hullgen import geometry_helper
 
+text_offset=(-5,-2.44,0)
+
 theCurveHelper = curve_helper.Curve_Helper()
 theCurveHelper.define_curve(11,1.2)
 
@@ -11,8 +13,9 @@ theCurveHelper.extrude_curve(1)
 
 wireframe = theCurveHelper.curve_object.modifiers.new(type="WIREFRAME", name="wireframe")
  
-info_text="A 2D curve with twist on the second axis"
-geometry_helper.add_info_text(info_text)
+newtxt=geometry_helper.add_info_text("A 2D curve")
+newtxt.parent=theCurveHelper.curve_object
+newtxt.location=text_offset
 
 # ==========================================
 theCurveHelper = curve_helper.Curve_Helper()
@@ -23,21 +26,26 @@ theCurveHelper.define_curve(11,1.2)
 
 theCurveHelper.generate_curve("mm")
 theCurveHelper.extrude_curve(1)
+theCurveHelper.curve_object.location.y=-3
 
 wireframe = theCurveHelper.curve_object.modifiers.new(type="WIREFRAME", name="wireframe")
 
-info_text="A 3D curve with twist on the third axis"
-geometry_helper.add_info_text(info_text)
+newtxt=geometry_helper.add_info_text("twist[0]=12 twist[1]=6")
+newtxt.parent=theCurveHelper.curve_object
+newtxt.location=text_offset
 
- 
+
 # =========================================
-
 theCurveHelper = curve_helper.Curve_Helper()
 theCurveHelper.asymmetry[1]=1
 theCurveHelper.define_curve(11,1.2)
 
 theCurveHelper.generate_curve("nn")
 theCurveHelper.extrude_curve(1)
+theCurveHelper.curve_object.location.y=3
 
 wireframe = theCurveHelper.curve_object.modifiers.new(type="WIREFRAME", name="wireframe")
  
+newtxt=geometry_helper.add_info_text("assymmetry[1]=1")
+newtxt.parent=theCurveHelper.curve_object
+newtxt.location=text_offset
