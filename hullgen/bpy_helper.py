41402e969285070fc17e13582d68fa329b60fe94
ek <ek@toaster>
2020-07-01 10:20:10 +0800

    add deselect function

--- hullgen/bpy_helper.py
+++ hullgen/bpy_helper.py
@@ -11,7 +11,16 @@ def select_object(theObject,selected):
 		bpy.context.view_layer.objects.active = None
 		theObject.select_set(state=False)
 
+def deselect_all_objects():
 
+	if bpy.context.active_object!=None:
+		print(bpy.context.active_object)
+
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+	bpy.ops.object.select_all(action='DESELECT')
+
+		
 
 def find_and_remove_object_by_name(objname):
 	for obj in bpy.data.objects:
